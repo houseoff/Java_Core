6073077c568af84e35943e01289a6fce0fdb3ac7
E. Khaustov <hrust656@gmail.com>
2023-12-27 21:40:14 +1000

    added seminar1 project

---
+++ seminar1/src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Args:");
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+    }
+}
+
+//-lambda
+//StreamAPI
+
+//git-flow
+
+//nested\inner classes
+
+//maven
+
+//--alg
+
+//-SOLID KISS DRY...
+//GRASP
+
+//:Singleton, Strategy, Fabric, Decorator
